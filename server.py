68ea8b3409392d0b31c816f7411177f017da2f7a
Dhanashree <kulkarni.dhanashree@gmail.com>
2026-02-10 19:23:12 -0500

    Server file changes

--- server.py
+++ server.py
@@ -1,24 +1,29 @@
+''' Executing this function initiates the application of Emotion Detection
+    to be executed over the Flask channel and deployed on
+    localhost:5001.
+'''
+# Import Flask, render_template, request from the flask pramework package
 from flask import Flask, render_template, request
+# Import the sentiment_analyzer function from the package created:
 from EmotionDetection.emotion_detection import emotion_detector
-
+#Initiate the flask app :
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """Function printing the dominant emotion."""
+    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get("textToAnalyze")
-
+ # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
-
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
-    
     result = (
         "For the given statement, the system response is "
         f"'anger': {anger}, "
@@ -28,7 +33,6 @@ def sent_detector():
         f"and 'sadness': {sadness}. "
         f"The dominant emotion is {dominant_emotion}."
     )
-
     return result
 
 @app.route("/")
